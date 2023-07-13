3d075d42cd04d7e05590ee7ff22089583c8a966d
Mayur0307 <mgupta433@gmail.com>
2023-07-14 01:10:01 +0530

    database issue

--- weatherforecastBackend/settings.py
+++ weatherforecastBackend/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'weatherforecastBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgresql://postgres:QMgB1eZyxJtcDwfbHSZx@containers-us-west-43.railway.app:7755/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'QMgB1eZyxJtcDwfbHSZx',
+        'HOST': 'containers-us-west-43.railway.app',
+        'PORT': 7755,
     }
 }
 
